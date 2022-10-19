dee4364385f88905d6147973feaf636a5a5b231f
Clarice Bouwer <clarice@simply.co.za>
2022-10-19 09:07:34 +0400

    Implement basic TODO functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import api from './utils/api';
+import uuid from 'node-uuid';
 
-function App() {
+const getTodos = async () => {
+  return await api.getTodos();
+};
+
+const addTodo = async (text) => {
+  const todo = await api.createTodo({
+    id: uuid.v1(),
+    completed: false,
+    text,
+  });
+
+  return {
+    id: todo.documentId,
+    completed: false,
+    text,
+  };
+};
+
+const deleteTodo = async (id) => {
+  const item = await api.deleteTodo(id);
+  return item.documentId;
+};
+
+const updateTodo = async (todo) => {
+  const item = await api.updateTodo(todo);
+  return item.documentId;
+};
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [item, setItem] = useState('');
+
+  useEffect(() => {
+    const loadTodos = async () => {
+      setTodos(await getTodos());
+    };
+    loadTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type="text"
+        placeholder="Enter something to do"
+        onChange={(e) => setItem(e.target.value)}
+      />
+      <button
+        onClick={async () => {
+          const todo = await addTodo(item);
+          setTodos([...todos, todo]);
+        }}
+      >
+        Add
+      </button>
+      {todos.length === 0 && <div>There is nothing to do.</div>}
+      {todos.length > 0 &&
+        todos.map((todo) => {
+          return (
+            <div key={todo.id}>
+              <button
+                onClick={async () => {
+                  const id = await deleteTodo(todo.id);
+                  setTodos(todos.filter((todo) => todo.id !== id));
+                }}
+              >
+                &times;
+              </button>{' '}
+              <label
+                style={{
+                  textDecoration: `${todo.completed ? 'line-through' : ''}`,
+                }}
+              >
+                <input
+                  type="checkbox"
+                  value={todo.id}
+                  onChange={async () => {
+                    const update = {
+                      ...todo,
+                      ...{ completed: !todo.completed },
+                    };
+                    const id = await updateTodo(update);
+                    setTodos(
+                      todos.map((todo) => (todo.id === id ? update : todo)),
+                    );
+                  }}
+                />
+                {todo.text}
+              </label>
+            </div>
+          );
+        })}
+    </>
   );
-}
+};
 
 export default App;
